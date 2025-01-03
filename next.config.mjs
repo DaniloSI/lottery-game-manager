9122c2dc1d4311019b0553ece830af7064f94fbf
Danilo de Oliveira <danilodeoliveira94@gmail.com>
2025-01-03 09:53:56 -0300

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+import "./src/env.mjs";
+
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    webpackBuildWorker: true,
+  },
+};
+
+export default bundleAnalyzer(nextConfig);
---
+++ tests/example.spec.ts
@@ -0,0 +1,23 @@
+import percySnapshot from "@percy/playwright";
+import { expect, test } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  await percySnapshot(page, "Homepage");
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
+});
